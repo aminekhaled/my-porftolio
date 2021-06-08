d07cd31b04b11290cf265ee5344ccb861d39a57e
aminekhaled <amine@git.com>
2021-06-08 15:59:29 +0100

    big commit

--- script.js
+++ script.js
@@ -4,19 +4,38 @@ var myHeader = document.getElementsByTagName('header')[0],
    pre_img = document.getElementById('pre-img'),
    myScrollTop = 0,
    Down = document.getElementsByClassName('down')[0],
-   Brithness_btn = document.getElementsByClassName('brithness')[0];
+   Brithness_btn = document.getElementsByClassName('brithness')[0],
+   nav = document.getElementsByTagName('nav')[0],
+   showNav = document.getElementsByClassName('show-nav-bar')[0],
+   hideNav = document.getElementsByClassName('hide-nav-bar')[0];
 
+   showNav.onclick = function() {
+      nav.style.display = "unset";
+      hideNav.style.display = "unset";
+   }
+   hideNav.onclick = function() {
+      nav.style.display = "none";
+      this.style.display = "none";
+   }
+
+
+
+window.onresize = function() {
+   if(this.innerWidth < 1000) {
+      console.log("Yes");
+   }
+}
 
 
    
 if(myScrollTop < 500) {
    setInterval(function() {
       if(pre_img.style.transform === "translateY(0px)") {
-         pre_img.style.transform = "translateY(7px)";
+         pre_img.style.transform = "translateY(10px)";
       }else {
-         pre_img.style.transform = "translateY(0px)"
+         pre_img.style.transform = "translateY(0px)";
       }
-   }, 1000);
+   }, 2000);
 } 
 
 
